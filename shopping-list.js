4ff89e37d76c439be31495cf694246303fa157a6
Kate <katshe18@gmail.com>
2023-11-08 10:04:13 +1300

    Added functionality to delete items, and to add/remove class when items are checked vs unchecked on the list.

--- shopping-list.js
+++ shopping-list.js
@@ -1,5 +1,6 @@
 const addItemButton = document.getElementById("addItemButton");
 const itemInput = document.getElementById("itemInput");
+let index = 0;
 
 addItemButton.addEventListener("click", addItem);
 
@@ -8,39 +9,37 @@ addItemButton.addEventListener("click", addItem);
 function addItem(e) {
   e.preventDefault();
 
-
-// Function to add the new item to our list:
+  // Function to add the new item to our list:
 
   const list = document.getElementById("list");
   let textInput = document.getElementById("itemInput").value;
   if (textInput.length > 0) {
     let newItem = document.createElement("li");
-    newItem.innerHTML =
-      `                        <label class="label">
+    newItem.innerHTML = `                        <label class="label" id="item-${index}">
       <input type="checkbox" class="checkbox"/>
       <span class="item">${textInput}</span>
       <span class="cross">×</span>
   </label>`;
     list.appendChild(newItem);
+
     document.getElementById("itemInput").value = "";
+
+    document
+      .getElementById(`item-${index}`)
+      .addEventListener("click", function (e) {
+        const target = e.target;
+        // console.log(e.target.classList)
+        if (target.classList.contains("cross")) {
+          newItem.remove();
+        }
+        if (target.classList.contains("checkbox")) {
+          console.log("clicked checkbox")
+        }
+      });
+    index++;
   }
-}
-
-// Function to remove an item when 'X' is clicked:
-
-const cross = document.getElementById("cross")
-cross.addEventListener("click", deleteItem)
-
-function deleteItem() {
-  document.removeElement("newItem")
 }
 
 
 // Function to change the item class if the checkbox is checked vs unchecked:
 
-const checkBox = document.getElementById("checkbox")
-if (checkBox.checked == true){
-  text.style.display = "block";
-} else {
-  text.style.display = "none";
-}
